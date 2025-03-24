fa7ede270e4a804783d502e0c02b3c5ee7de0612
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-03-24 12:36:08 +0500

    html

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Comments)
+admin.site.register(Rating)
