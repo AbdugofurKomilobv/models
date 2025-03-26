7edf2f5dce55be93aba1ed3c7c3aec661951f55b
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-03-26 05:55:18 +0500

    user app

--- conf/settings.py
+++ conf/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'user_app',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]  # assets emas!
